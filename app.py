f1a17b418e9182bd91c7b61a358fb5f64876675a
hugoborini <hugo.borini@hetic.net>
2021-07-19 11:02:09 +0200

    add some comment :art:

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+#import vendor
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
+#end import vendor
 
+#import controller
 from controller import status as status
-
+#end import controller
 
 app = Flask(__name__)
 api = Api(app)
